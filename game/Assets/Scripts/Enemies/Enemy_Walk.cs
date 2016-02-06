7ed331d8c515a15d1d7852febb8786148abf34a5
Erdrik <s.ballard-adams@outlook.com>
2016-02-06 19:53:24 +0000

    Enemies avoid obstacles and destroy themselves
    Upon colliding, an enemy moves a distance in a diverted direction to
    avoid the obstacle, if the object has an obstacle script attached.
    If the object has a health script attached the enemy will continue to
    collide with it (attacking it).

--- game/Assets/Scripts/Enemies/Enemy_Walk.cs
+++ game/Assets/Scripts/Enemies/Enemy_Walk.cs
@@ -17,6 +17,7 @@ public class Enemy_Walk : MonoBehaviour {
     {
         target_ = target_location;
     }
+    public bool diverted_ = false;
 
     private float actual_speed_ = 1.0f;
 
@@ -32,33 +33,75 @@ public class Enemy_Walk : MonoBehaviour {
         {
             Update_Velocity();
 
-            this.transform.position = this.transform.position + velocity_;
+            Vector3 new_position = this.transform.position + velocity_;
             if (debug_mode_)
             {
                 // Movement line
-                Debug.DrawLine(this.transform.position, target_position_, Color.red, 1.0f);
-                Debug.DrawLine(this.transform.position, this.transform.position + velocity_, Color.blue, 1.0f);
+                Debug.DrawLine(new_position, target_position_, Color.red, 0.1f);
+                Debug.DrawLine(new_position, new_position + velocity_, Color.blue, 0.1f);
+            }
+            this.transform.position = new_position;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Target_Location arrived = collision.gameObject.GetComponent<Target_Location>();
+        if (arrived == null) {
+            // There is nothing to do, really
+        } else {
+            // We've reached the location!
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionStay(Collision collision) {
+        if (!diverted_) {
+            Health target_health = collision.gameObject.GetComponent<Health>();
+            if (target_health == null) {
+                // The enemy is non-destructible
+                // We need to change direction
+
+                int direction = Random.Range(1, 3);
+                float divert_by = (collision.collider.bounds.size.z + this.collider.bounds.size.z);
+                if (direction > 1) {
+                    target_position_ = this.transform.position + new Vector3(0.0f, 0.0f, divert_by);
+                } else {
+                    target_position_ = this.transform.position + new Vector3(0.0f, 0.0f, -divert_by);
+                }
+
+                diverted_ = true;
+            } else {
+                // The enemy is destructible
+                // KEEP GOING!
+                Debug.Log("COLLIDED WITH DESTRUCTIBLE OBJECT");
             }
         }
     }
 
-    void OnCollisionEnter (Collision collision) {
-        Health target_health = collision.gameObject.GetComponent<Health>();
-        if (target_health == null) {
-            // The enemy is non-destructible
-            // We need to change direction
-        } else {
-            // The enemy is destructible
-            // KEEP GOING!
-            Debug.Log("COLLIDED WITH DESTRUCTIBLE OBJECT");
-        }
-    }
-
-    private void Update_Velocity()
-    {
+    private void Update_Velocity() {
         direction_ = target_position_ - this.transform.position;
         direction_.Normalize();
 
-        velocity_ = direction_ * actual_speed_;
+        if (diverted_) {
+            // We're having to dodge stuff at the moment
+            float distance = Vector3.Distance(this.transform.position, target_position_);
+            if (distance < actual_speed_) {
+                // We've just reached the diverted destination, so let's re-target
+                velocity_ = direction_ * distance;
+                target_position_ = target_.transform.position;
+                diverted_ = false;
+            } else {
+                // We're still travelling to the destination
+                velocity_ = direction_ * actual_speed_;
+            }
+        } else {
+            float distance = Vector3.Distance(this.transform.position, target_position_);
+            if (distance < actual_speed_) {
+                velocity_ = direction_ * distance;
+                // ARRIVED AT TARGET LOCATION
+            } else {
+                velocity_ = direction_ * actual_speed_;
+            }
+        }
     }
 }
--- game/Assets/Scripts/Enemies/Spawn_Location.cs
+++ game/Assets/Scripts/Enemies/Spawn_Location.cs
@@ -7,19 +7,25 @@ public class Spawn_Location : MonoBehaviour {
 
     public Target_Location target_location_;
 
+    public float spawn_delay_ = 1.0f;
+    public float next_spawn_time_ = 0.0f;
+
 	// Use this for initialization
 	void Start () {
-        Spawn_Enemy();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float current_time = Time.time;
+        if (current_time > next_spawn_time_) {
+            Spawn_Enemy();
+            next_spawn_time_ = current_time + spawn_delay_;
+        }
 	}
 
     public void Spawn_Enemy()
     {
-        Debug.Log("SPAWNING ENEMY");
         GameObject new_enemy = (GameObject) GameObject.Instantiate(enemy_, new Vector3(this.transform.position.x, 0.0f, this.transform.position.z), this.transform.rotation);
         Enemy_Walk walk = new_enemy.GetComponent<Enemy_Walk>();
         walk.Set_Target(target_location_);
