6abe66a61fe6c6faae96c6e284e4fc60fb035554
Jordan Davidson <m2027291@live.tees.ac.uk>
2016-02-06 19:52:08 +0000

    Projectiles are now destroyed after 8 seconds

---
+++ game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    private float timeAlive;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeAlive > 8.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        timeAlive += Time.deltaTime;
+	}
+}
