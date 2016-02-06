efeffcd80b5ce76b8792b328f6b7113ff3b508f0
Erdrik <s.ballard-adams@outlook.com>
2016-02-06 17:14:12 +0000

    Enemy Spawn and Walk
    Enemies can be spawned by spawn locations.
    Enemies walk toward the target location.
    Enemy speed is determined by how long you want them to "as the crow
    flies" reach the target upon spawn in seconds.

---
+++ game/Assets/Scripts/Enemies/Enemy_Walk.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Walk : MonoBehaviour {
+
+    public bool debug_mode_;
+
+    public float minimum_time_taken_ = 1.0f;
+    public Vector3 velocity_;
+    public Vector3 direction_;
+
+    public bool is_moving_ = true;
+
+    public Target_Location target_;
+    public void Set_Target(Target_Location target_location)
+    {
+        target_ = target_location;
+    }
+
+    private float actual_speed_ = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+        actual_speed_ = Vector3.Distance(this.transform.position, target_.transform.position) / (60.0f * minimum_time_taken_);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (is_moving_)
+        {
+            Update_Velocity();
+            if (debug_mode_)
+            {
+                Debug.DrawLine(this.transform.position, this.transform.position + velocity_);
+            }
+            this.transform.position = this.transform.position + velocity_;
+        }
+    }
+
+    void Fixed_Update() {
+
+    }
+
+    private void Update_Velocity()
+    {
+        direction_ = target_.transform.position - this.transform.position;
+        direction_.Normalize();
+
+        velocity_ = direction_ * actual_speed_;
+    }
+}
---
+++ game/Assets/Scripts/Enemies/Spawn_Location.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn_Location : MonoBehaviour {
+
+    public GameObject enemy_;
+
+    public Target_Location target_location_;
+
+	// Use this for initialization
+	void Start () {
+        Spawn_Enemy();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Spawn_Enemy()
+    {
+        Debug.Log("SPAWNING ENEMY");
+        GameObject new_enemy = (GameObject) GameObject.Instantiate(enemy_, new Vector3(this.transform.position.x, 0.0f, this.transform.position.z), this.transform.rotation);
+        Enemy_Walk walk = new_enemy.GetComponent<Enemy_Walk>();
+        walk.Set_Target(target_location_);
+    }
+}
