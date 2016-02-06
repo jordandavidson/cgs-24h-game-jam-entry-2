744780441a4f66f70a11c7752e0caf00dfb0c034
Jordan Davidson <m2027291@live.tees.ac.uk>
2016-02-06 17:09:25 +0000

    Projectile firing working and shield added to Player 2

---
+++ game/Assets/Scripts/FireProjectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FireProjectile : MonoBehaviour {
+
+    public GameObject projectilePrefab;
+
+    private Player player;
+
+	// Use this for initialization
+	void Start () {
+
+        player = gameObject.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (player.ID == 1 && Input.GetKeyUp(KeyCode.Space) || (player.ID == 2 && Input.GetKeyUp(KeyCode.RightShift)))
+        {
+            if (!player)
+                return;
+
+            var projectile = Instantiate(projectilePrefab,
+                                         transform.position + new Vector3(1.0f, 0.0f, 0.0f),
+                                         Quaternion.identity) as GameObject;
+
+            var rigidBody = projectile.GetComponent<Rigidbody>();
+            rigidBody.velocity = new Vector3(10.0f, 0.0f, 0.0f);
+        }
+	}
+}
