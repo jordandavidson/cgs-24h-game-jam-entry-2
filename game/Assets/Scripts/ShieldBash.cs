b72ba8a926797c5eaf5e05b2f0d207382fad9cfe
Jordan Davidson <m2027291@live.tees.ac.uk>
2016-02-06 19:04:29 +0000

    Fixed issue of players sinking into the floor

---
+++ game/Assets/Scripts/ShieldBash.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldBash : MonoBehaviour {
+
+    public GameObject shield;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    public void Bash()
+    {
+
+
+    }
+}
