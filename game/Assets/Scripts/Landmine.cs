38f8daf94506006e03cebb35b7685f50e1f5b9ba
Erdrik <s.ballard-adams@outlook.com>
2016-02-07 07:44:47 +0000

    Wizard now has landmine ability
    Wizard can place down a landmine using the same kind of building
    blueprint system as the builder.

--- game/Assets/Scripts/Landmine.cs
+++ game/Assets/Scripts/Landmine.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Attack : MonoBehaviour {
+public class Landmine : MonoBehaviour {
 
-    public Health.Faction faction_;
-    public int damage_;
+    public Health.Faction faction_ = Health.Faction.PLAYER;
+    public int damage_ = 5;
+
+    public GameObject particles_;
 
 	// Use this for initialization
 	void Start () {
@@ -16,8 +18,8 @@ public class Attack : MonoBehaviour {
 	
 	}
 
-    void OnCollisionEnter(Collision collision) {
-        Attack_Target(collision.gameObject.GetComponent<Health>());
+    void OnTriggerEnter(Collider collider) {
+        Attack_Target(collider.gameObject.GetComponent<Health>());
     }
 
     private void Attack_Target(Health target_health) {
@@ -28,6 +30,10 @@ public class Attack : MonoBehaviour {
                 // Is the same faction, so should not hit
             } else {
                 target_health.Edit_Health(-damage_);
+                if (particles_ != null) {
+                    GameObject.Instantiate(particles_, this.transform.position, Quaternion.identity);
+                }
+                GameObject.Destroy(this.gameObject);
             }
         }
     }
--- game/Assets/Scripts/Magic.cs
+++ game/Assets/Scripts/Magic.cs
@@ -6,6 +6,9 @@ public class Magic : MonoBehaviour {
     private Player player;
 
     public Healing_Zone healing_zone_;
+    public PreConstruction pre_landmine_;
+
+    private bool blueprinting_ = false;
 
     // Use this for initialization
     void Start () {
@@ -14,22 +17,29 @@ public class Magic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (player.ID == 1 && Input.GetKeyDown(KeyCode.X) || (player.ID == 2 && Input.GetKeyDown(KeyCode.Slash))) {
-            if (healing_zone_ != null) {
-                healing_zone_.Activate(Health.Faction.PLAYER);
-                player.StopMovement();
-            }
-        } else if (player.ID == 1 && Input.GetKeyDown(KeyCode.Z) || (player.ID == 2 && Input.GetKeyDown(KeyCode.Greater))) {
-
-        }
 
         if (player.ID == 1 && Input.GetKeyUp(KeyCode.X) || (player.ID == 2 && Input.GetKeyUp(KeyCode.Slash))) {
             if (healing_zone_ != null) {
                 healing_zone_.Deactivate();
                 player.StartMovement();
             }
-        } else if (player.ID == 1 && Input.GetKeyUp(KeyCode.Z) || (player.ID == 2 && Input.GetKeyUp(KeyCode.Greater))) {
+        }
 
+        if (player.ID == 1 && Input.GetKeyDown(KeyCode.X) || (player.ID == 2 && Input.GetKeyDown(KeyCode.Slash))) {
+            if (healing_zone_ != null) {
+                healing_zone_.Activate(Health.Faction.PLAYER);
+                player.StopMovement();
+            }
+        }
+
+        if (blueprinting_) {
+            if (player.ID == 1 && Input.GetKeyUp(KeyCode.Z) || (player.ID == 2 && Input.GetKeyUp(KeyCode.Greater))) {
+                pre_landmine_.Construct();
+                blueprinting_ = false;
+            }
+        } else if (player.ID == 1 && Input.GetKeyDown(KeyCode.Z) || (player.ID == 2 && Input.GetKeyDown(KeyCode.Greater))) {
+            pre_landmine_.Blueprint();
+            blueprinting_ = true;
         }
     }
 }
