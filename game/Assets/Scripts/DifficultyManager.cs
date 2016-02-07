ae1767f4606fe24ebfa04cff9a15182273f3ccc6
Jordan Davidson <m2027291@live.tees.ac.uk>
2016-02-07 00:14:40 +0000

    Prefabs for Managers and HUD

---
+++ game/Assets/Scripts/DifficultyManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour {
+
+    public GameObject gameManager;
+    public GameObject gameTimer;
+
+    private GameManager game;
+    private Timer timer;
+
+    void Start () {
+
+        game = gameManager.GetComponent<GameManager>();
+        timer = gameTimer.GetComponent<Timer>();
+	}
+	
+	void Update () {
+
+        if (timer.gameTime > 10.0f)
+        {
+
+
+        }
+	}
+
+    private void Reset()
+    {
+
+    }
+}
