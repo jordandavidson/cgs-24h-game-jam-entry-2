ca4b91ba8fc390015f7e1035a498185d813628a2
Jordan Davidson <m2027291@live.tees.ac.uk>
2016-02-07 11:27:29 +0000

    Merged character spawn changes into game

---
+++ game/Assets/CharacterSelectManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelectManager : MonoBehaviour {
+
+    public GameObject Hunter;
+    public GameObject Wizard;
+    public GameObject Builder;
+    public GameObject Knight;
+
+    public GameObject spawnLocation;
+    public int playerID;
+
+    private bool characterSelected;
+    private GameObject currentCharacter;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    public void SelectHunter()
+    {
+        InstantiateCharacter(Hunter);
+    }
+
+    public void SelectWizard()
+    {
+        InstantiateCharacter(Wizard);
+    }
+
+    public void SelectBuilder()
+    {
+        InstantiateCharacter(Builder);
+    }
+
+    public void SelectKnight()
+    {
+        InstantiateCharacter(Knight);
+    }
+
+    private void InstantiateCharacter(Object character)
+    {
+        if (currentCharacter) {
+
+            Destroy(currentCharacter);
+        }
+
+        currentCharacter = Instantiate(character, spawnLocation.transform.position, Quaternion.identity) as GameObject;
+        currentCharacter.SetActive(true);
+        var player = currentCharacter.GetComponent<Player>();
+        player.ID = playerID;
+    }
+}
